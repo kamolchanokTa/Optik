3cd11a370dae1824dcbad00e81414a5a6400d4e2
WINCTRL-GFST2VR\Chuck <echo770719@gmail.com>
2018-05-16 20:50:25 +0200

    add update address and credit components; add register component for temp user.

--- src/app/app.router.ts
+++ src/app/app.router.ts
@@ -3,11 +3,20 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { ProductOverviewComponent } from './components/product-overview/product-overview.component';
 import { RegisterationComponent } from './components/registeration/registeration.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { UpdateAddressComponent } from './components/update-address/update-address.component';
+import { UpdateCreditComponent } from './components/update-credit/update-credit.component';
+
 
 const appRoutes: Routes = [
     { path: '', redirectTo: '/product-overview', pathMatch: 'full' },
     { path: 'product-overview', component: ProductOverviewComponent },
     { path: 'registeration', component: RegisterationComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: 'update-address', component: UpdateAddressComponent },
+    { path: 'update-credit', component: UpdateCreditComponent },
 ]
 
 @NgModule({
--- src/app/components.register.ts
+++ src/app/components.register.ts
@@ -2,6 +2,10 @@
 import { AppComponent } from './app.component';
 import { ProductOverviewComponent } from './components/product-overview/product-overview.component';
 import { RegisterationComponent } from './components/registeration/registeration.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { UpdateAddressComponent } from './components/update-address/update-address.component';
+import { UpdateCreditComponent } from './components/update-credit/update-credit.component';
 
 //import services
 import { ProductService } from './services/product.service';
@@ -9,6 +13,6 @@ import { UserService } from './services/user.service';
 
 export const bootstrap = [AppComponent];
 
-export const componentList = [AppComponent, ProductOverviewComponent, RegisterationComponent];
+export const componentList = [AppComponent, ProductOverviewComponent, RegisterationComponent, LoginComponent, RegisterComponent, UpdateAddressComponent, UpdateCreditComponent];
 
 export const serviceList = [ProductService, UserService];
